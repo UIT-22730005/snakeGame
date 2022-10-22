7a5b408ba99d525fe414d8337ed6a32b0067e399
UIT-22730004 <22730004@ms.uit.edu.vn>
2022-10-22 23:46:48 +0700

    add Point and gotoxy function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+struct Point{
+    int x,y;
+};
+
+void gotoxy( int column, int line )
+  {
+  COORD coord;
+  coord.X = column;
+  coord.Y = line;
+  SetConsoleCursorPosition(
+    GetStdHandle( STD_OUTPUT_HANDLE ),
+    coord
+    );
+  }
 
 int main()
 {
