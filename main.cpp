ff028e1d2e3c143027667dd4b7f2ef55582a1dc4
UIT-22730001 <22730001@ms.uit.edu.vn>
2022-10-23 00:00:42 +0700

    create gotoxy function

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ struct Point{
     int x,y;
 };
 
+void gotoxy( int column, int line )
+  {
+  COORD coord;
+  coord.X = column;
+  coord.Y = line;
+  SetConsoleCursorPosition(
+    GetStdHandle( STD_OUTPUT_HANDLE ),
+    coord
+    );
+  }
 
 int main()
 {
